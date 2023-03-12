f9a81135d3063a4fdc36da603f068e13e847f44b
Iqbal <iamhasan9501@gmail.com>
2023-03-12 11:43:01 +0600

    f

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ async function run() {
   try {
     const usersCollection = client.db("nodeMongoCRUD").collection("users");
 
-    app.get("/", (req, res) => {
-      res.send("node server running...");
-    });
-
     app.get("/users", async (req, res) => {
       const query = {};
       const cursor = usersCollection.find(query);
@@ -75,12 +71,12 @@ async function run() {
   }
 }
 
-await run().catch(console.dir);
+run().catch(console.dir);
 
 // run().catch((err) => console.log(err));
-// app.get("/", (req, res) => {
-//   res.send("node server running...");
-// });
+app.get("/", (req, res) => {
+  res.send("node server running...");
+});
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
